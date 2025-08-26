5c4f29b7979fb2c3897a4838705e3462a21a7d55
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-08-26 21:41:24 +0400

    Processes

--- SP 01. Processes/Program.cs
+++ SP 01. Processes/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+// Processes
+
+
+//Process.Start("calc");
+//Process.Start("calc");
+//Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+
+//Console.WriteLine(Process.GetCurrentProcess().ProcessName);
+//Console.WriteLine(Process.GetCurrentProcess().Id);
+//Console.WriteLine(Process.GetCurrentProcess().BasePriority);
+
+//var process = Process.GetProcessById(36308); // Process Id dinamicheski videlyayetsya OS i kajdiy raz mojet bit raznim
+
+//Console.ReadLine();
+
+//process.Kill();
+
+//var processes = Process.GetProcessesByName("CalculatorApp");
+
+//foreach (var proc in processes)
+//{
+//    Console.WriteLine($"{proc.Id} - {proc.ProcessName} - {proc.Threads.Count}");
+//    proc.Kill();
+//}
+
+//var allProcesses = Process.GetProcesses();
+
+//foreach (var proc in allProcesses)
+//{
+//    Console.WriteLine($"{proc.Id} - {proc.ProcessName} - {proc.Threads.Count}");
+//}
+
+//var current = Process.GetCurrentProcess();
+//current.PriorityClass = ProcessPriorityClass.High;
+
+ProcessStartInfo process = new ProcessStartInfo("calc");
+
+Process.Start(process);
+Console.WriteLine(process.WorkingDirectory);
+Console.ReadLine();
