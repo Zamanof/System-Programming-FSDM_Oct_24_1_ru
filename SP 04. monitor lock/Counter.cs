93f332a5d7ac3777711322c77397fa23b3e3f8a3
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-09-02 21:52:23 +0400

    lock monitor

---
+++ SP 04. monitor lock/Counter.cs
@@ -0,0 +1,6 @@
+﻿
+class Counter
+{
+    public static int count = 0;
+    public static int even = 0;
+}
---
+++ SP 04. monitor lock/Program.cs
@@ -0,0 +1,213 @@
+﻿#region Albahari tricks
+// First Trick
+//for (int i = 0; i < 10; i++)
+//{
+//    new Thread(() =>
+//    {
+//        Console.WriteLine(i);
+//    }).Start();
+//}
+
+
+//for (int i = 0; i < 10; i++)
+//{
+//    int temp = i;
+//    new Thread(() =>
+//    {
+//        Console.WriteLine(temp);
+//    }).Start();
+//}
+
+// Second Trick
+//string name = "Nadir";
+//Thread thread1 = new(() =>
+//{
+//    Console.WriteLine(name);
+//});
+
+//name = "Zaman";
+
+//Thread thread2 = new(() =>
+//{
+//    Console.WriteLine(name);
+//});
+
+//thread1.Start();
+//thread2.Start();
+#endregion
+
+// Critical section - obrosheniye thread-ov na odnu i tu ju pamyat(resurs)
+
+#region Critical section problem
+//Thread[] threads = new Thread[5];
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i] = new Thread(() =>
+//    {
+//        for (int j = 0; j < 1000000; j++)
+//        {
+//            Counter.count++;
+//        }
+//    });
+//}
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i].Start();
+//}
+
+////Console.WriteLine(Counter.count);
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i].Join();
+//}
+//Console.WriteLine(Counter.count);
+#endregion
+
+#region Interlocked
+//Thread[] threads = new Thread[5];
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i] = new Thread(() =>
+//    {
+//        for (int j = 0; j < 1000000; j++)
+//        {
+//            Interlocked.Increment(ref Counter.count);
+//        }
+//    });
+//}
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i].Start();
+//}
+
+////Console.WriteLine(Counter.count);
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i].Join();
+//}
+//Console.WriteLine(Counter.count);
+#endregion
+
+#region Interlocked problem
+//Thread[] threads = new Thread[5];
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i] = new Thread(() =>
+//    {
+//        for (int j = 0; j < 1000000; j++)
+//        {
+//            Interlocked.Increment(ref Counter.count);
+
+//            if (Counter.count % 2 == 0)
+//            {
+//                Interlocked.Increment(ref Counter.even);
+//            }
+//        }
+//    });
+//}
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i].Start();
+//}
+
+////Console.WriteLine(Counter.count);
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i].Join();
+//}
+//Console.WriteLine(Counter.count);
+//Console.WriteLine(Counter.even);
+#endregion
+
+#region Monitor
+//Thread[] threads = new Thread[5];
+//object obj = new();
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i] = new Thread(() =>
+//    {
+//        for (int j = 0; j < 1000000; j++)
+//        {
+//            Monitor.Enter(obj);
+//            try
+//            {
+//                Counter.count++;
+
+//                if (Counter.count % 2 == 0)
+//                {
+//                    Counter.even++;
+//                }
+//            }
+//            finally
+//            {
+
+//                Monitor.Exit(obj);
+//            }
+
+//        }
+//    });
+//}
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i].Start();
+//}
+
+////Console.WriteLine(Counter.count);
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i].Join();
+//}
+//Console.WriteLine(Counter.count);
+//Console.WriteLine(Counter.even);
+#endregion
+
+#region lock
+//Thread[] threads = new Thread[5];
+//object obj = new();
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i] = new Thread(() =>
+//    {
+//        for (int j = 0; j < 1000000; j++)
+//        {
+//            // lock - monitor syntax sugar 
+//            lock (obj)
+//            {
+//                Counter.count++;
+
+//                if (Counter.count % 2 == 0)
+//                {
+//                    Counter.even++;
+//                }
+//            }
+//        }
+//    });
+//}
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i].Start();
+//}
+
+////Console.WriteLine(Counter.count);
+
+//for (int i = 0; i < threads.Length; i++)
+//{
+//    threads[i].Join();
+//}
+//Console.WriteLine(Counter.count);
+//Console.WriteLine(Counter.even);
+#endregion
+
+
