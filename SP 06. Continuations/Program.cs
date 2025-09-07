85cc97475f2549ee3fca9581752313128b16f4c0
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-09-07 21:56:24 +0400

    TPL

---
+++ SP 06. Continuations/Program.cs
@@ -0,0 +1,152 @@
+﻿// Continuations
+#region ContinueWith OnlyOnRanToCompletetaion
+//var firstTask = new Task<int>(() => TaskMethod("First Task", 3));
+//var secondTask = new Task<int>(() => TaskMethod("Second Task", 4));
+
+//firstTask.ContinueWith((t) =>
+//{
+//    Console.WriteLine("ContinueWith task Start");
+//    Console.WriteLine($"Task result: {t.Result}");
+//    Console.WriteLine($@"Id:             {Thread.CurrentThread.ManagedThreadId}
+//IsBackground:   {Thread.CurrentThread.IsBackground}
+//IsThreadPool:   {Thread.CurrentThread.IsThreadPoolThread}");
+//}, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+//secondTask.ContinueWith((t) =>
+//{
+//    OtherMethod();
+//}, TaskContinuationOptions.NotOnCanceled | TaskContinuationOptions.NotOnFaulted);
+
+//firstTask.Start();
+//secondTask.Start();
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"Main thread - {i}");
+//    Thread.Sleep(10);
+//}
+
+//Task.WaitAll(firstTask, secondTask);
+//Console.WriteLine("End");
+#endregion
+
+#region ContinueWith OnlyOnFaulted
+//try
+//{
+//    var firstTask = new Task<int>(() => TaskMethod("First Task", 3));
+
+//    firstTask.ContinueWith((t) =>
+//    {
+//        Console.WriteLine("ContinueWith task Start");
+//        Console.WriteLine($@"Id:             {Thread.CurrentThread.ManagedThreadId}
+//IsBackground:   {Thread.CurrentThread.IsBackground}
+//IsThreadPool:   {Thread.CurrentThread.IsThreadPoolThread}");
+//    }, TaskContinuationOptions.OnlyOnFaulted);
+
+
+
+//    firstTask.Start();
+
+
+
+//}
+//catch (Exception ex)
+//{
+
+//    Console.WriteLine(ex.Message);
+//}
+//Console.ReadKey();
+#endregion
+
+#region ContinueWith with Thread
+//var firstTask = new Task<int>(() => TaskMethod("First Task", 3));
+
+//firstTask.ContinueWith((t) =>
+//{
+//    Console.WriteLine("ContinueWith task Start");
+//    Console.WriteLine($@"Id:             {Thread.CurrentThread.ManagedThreadId}
+//IsBackground:   {Thread.CurrentThread.IsBackground}
+//IsThreadPool:   {Thread.CurrentThread.IsThreadPoolThread}");
+//    Console.WriteLine("ContinueWith task End");
+//}, TaskContinuationOptions.OnlyOnRanToCompletion | TaskContinuationOptions.LongRunning);
+//firstTask.Start();
+
+//Console.ReadLine();
+#endregion
+
+#region ContinueWith ExecuteSyncronously
+//var firstTask = new Task<int>(() => TaskMethod("First Task", 3));
+
+//firstTask.ContinueWith((t) =>
+//{
+//    Console.WriteLine("ContinueWith task Start");
+//    Console.WriteLine($@"Id:             {Thread.CurrentThread.ManagedThreadId}
+//IsBackground:   {Thread.CurrentThread.IsBackground}
+//IsThreadPool:   {Thread.CurrentThread.IsThreadPoolThread}");
+//    Console.WriteLine("ContinueWith task End");
+//}, TaskContinuationOptions.OnlyOnRanToCompletion | TaskContinuationOptions.ExecuteSynchronously);
+//firstTask.Start();
+
+//Console.ReadLine();
+#endregion
+
+#region Task.Status
+// Created
+// WaitingForActivation
+// WaitingToRun
+// Running
+// WaitingForChildrenToComplete
+// RanToCompletion
+// Canceled
+// Faulted
+var firstTask = new Task<int>(() => TaskMethod("First Task", 3));
+try
+{
+    Console.WriteLine(firstTask.Status);
+    firstTask.Start();
+    while (true)
+    {
+        Console.WriteLine(firstTask.Status);
+        Thread.Sleep(100);
+        if (firstTask.IsCompleted) break;
+    }
+    firstTask.Wait();
+    Console.WriteLine(firstTask.Status);
+}
+catch (Exception)
+{
+    Console.WriteLine(firstTask.Status);
+}
+
+
+#endregion
+
+
+int TaskMethod(string message, int second)
+{
+    Console.WriteLine($"Task - {message} start.");
+    Console.WriteLine($@"
+Id:             {Thread.CurrentThread.ManagedThreadId}
+IsBackground:   {Thread.CurrentThread.IsBackground}
+IsThreadPool:   {Thread.CurrentThread.IsThreadPoolThread}
+");
+    Thread.Sleep(TimeSpan.FromSeconds(second));
+    throw new Exception("Any exception");
+
+    Console.WriteLine($"Task - {message} end.");
+    return second * 10;
+}
+
+void OtherMethod()
+{
+    Console.WriteLine("Other method start.");
+    Console.WriteLine($@"
+Id:             {Thread.CurrentThread.ManagedThreadId}
+IsBackground:   {Thread.CurrentThread.IsBackground}
+IsThreadPool:   {Thread.CurrentThread.IsThreadPoolThread}
+");
+
+
+    Console.WriteLine("Other method end.");
+
+}
---
+++ SP 06. TPL/Program.cs
@@ -0,0 +1,45 @@
+﻿// TPL - Task Parallel Library
+
+// Thread -> ThreadPool -> TPL
+
+
+Task task1 = new Task(() =>
+{
+    TaskMethod("Task1");
+});
+Task task2 = new Task(() =>
+{
+    TaskMethod("Task2");
+});
+
+Task task3 = Task.Run(() =>
+{
+    TaskMethod("Task3");
+});
+
+Task task4 = Task.Factory.StartNew(() =>
+{
+    TaskMethod("Task4");
+});
+
+Task task5 = new Task(() =>
+{
+    TaskMethod("Task5");
+},TaskCreationOptions.LongRunning);
+
+
+task1.Start();
+task2.Start();
+task5.Start();
+
+
+void TaskMethod(string message)
+{
+    Console.WriteLine($@"
+Name:           {message}
+Id:             {Thread.CurrentThread.ManagedThreadId}
+IsBackground:   {Thread.CurrentThread.IsBackground}
+IsThreadPool:   {Thread.CurrentThread.IsThreadPoolThread}
+");
+}
+
